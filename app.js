4b36c3022b0adf4d4b988cae5fdc84eaf9ae2efd
gorenNoa <noagoren@post.bgu.ac.il>
2020-05-05 02:35:42 +0300

    about finished

--- app.js
+++ app.js
@@ -209,13 +209,13 @@ function open_about() {
     // Get the modal
     var modal = document.getElementById("myModal");
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    // var btn = document.getElementById("myBtn");
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
     // When the user clicks the button, open the modal
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
+    // btn.onclick = function () {
+    //     modal.style.display = "block";
+    // }
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
@@ -233,6 +233,7 @@ function open_about() {
             modal.style.display = "none";
         }
     }
+    modal.style.display = "block";
 }
 
 function myFunction() {
